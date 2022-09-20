51fca9f65253369071220f855e8f771924aedc24
Rita Zhang <ruijia1217@gmail.com>
2022-09-20 15:24:39 -0600

    project skeleton is set up

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div>Bot name: {{ bot_name }}</div>
+    <div>Bot rating: {{ bot_rating }}</div>
   </div>
   <router-view/>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+  export default {
+    name: "App",
+    setup: () => {
+      let bot_name = ref("");
+      let bot_rating = ref("");
+
+      $.ajax({
+        url: "http://127.0.0.1:8080/pk/getbotinfo/",
+        type: "get",
+        success: resp => {
+          // console.log(resp);
+          bot_name.value = resp.name;
+          bot_rating.value = resp.rating;
+        }
+      });
+
+      return {
+        bot_name,
+        bot_rating
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("./assets/background.png");
+  background-size: cover;
 }
 </style>
