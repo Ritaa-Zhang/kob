505c71f74b06f46f6e0aaad98615286ef7a93e67
Rita Zhang <ruijia1217@gmail.com>
2022-09-20 20:42:30 -0600

    navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,42 +1,24 @@
 <template>
-  <div>
-    <div>Bot name: {{ bot_name }}</div>
-    <div>Bot rating: {{ bot_rating }}</div>
-  </div>
+  <NavBar />
   <router-view/>
 </template>
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap"
 
-  export default {
-    name: "App",
-    setup: () => {
-      let bot_name = ref("");
-      let bot_rating = ref("");
-
-      $.ajax({
-        url: "http://127.0.0.1:8080/pk/getbotinfo/",
-        type: "get",
-        success: resp => {
-          // console.log(resp);
-          bot_name.value = resp.name;
-          bot_rating.value = resp.rating;
-        }
-      });
-
-      return {
-        bot_name,
-        bot_rating
-      }
-    }
+export default {
+  components: {
+    NavBar
   }
+}
 </script>
 
 <style>
 body {
   background-image: url("./assets/background.png");
   background-size: cover;
+  /* background-repeat: no-repeat; */
 }
 </style>
