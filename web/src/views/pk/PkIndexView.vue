1bbe5d29135556f2e02633bb640d01510f82adf1
Rita Zhang <ruijia1217@gmail.com>
2022-09-22 14:51:11 -0600

    gamemap, walls and obstacles

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,15 +1,13 @@
 <template>
-    <ContentField>
-        pk
-    </ContentField>
+    <PlayGround />
 </template>
 
 <script>
-import ContentField from '../../components/ContentField.vue'
+import PlayGround from '../../components/PlayGround.vue'
 
 export default {
     components: {
-        ContentField
+        PlayGround
     }
 }
 </script>
