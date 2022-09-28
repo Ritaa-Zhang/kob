67bcd3bb2527147ead0f2cc11c25603ab02d9ae5
Rita Zhang <ruijia1217@gmail.com>
2022-09-27 20:44:40 -0600

    configure Mysql, SpringBoot, Spring Security

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,20 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("p1"));
+        System.out.println(passwordEncoder.encode("p2"));
+        System.out.println(passwordEncoder.encode("p3"));
+        System.out.println(passwordEncoder.encode("p4"));
+//        System.out.println(passwordEncoder.matches("p2", "$2a$10$KTWBvIN/uQB8.A0hf/qzLulKelgrjSjrxjtyb.HDVbeZFBDVfhma6"));
     }
 
 }
