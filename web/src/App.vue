f9586549f5ebbdf69c5510c743050166dab82031
Rita Zhang <ruijia1217@gmail.com>
2022-09-29 19:53:38 -0600

    Implement Jwt authentication and login module

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,13 +4,61 @@
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/js/bootstrap";
+// import $ from 'jquery';
 
 export default {
   components: {
     NavBar
+  },
+
+  setup() {
+    // $.ajax({
+    //   url: "http://127.0.0.1:8080/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: 'zrj',
+    //     password: 'p2',
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp); 
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:8080/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmNGI5OGM0MGIwNDc0YzBlODM5ZDllYTA0MjkwYzM2MCIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY2NDQxMjIyMCwiZXhwIjoxNjY1NjIxODIwfQ.3PCZSwUJ-0Yj6ct0PBf2vwVSfNYYhI9UIpHbjpYHsyQ",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp); 
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:8080/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: 'zrj2',
+    //     password: 'pzrj',
+    //     confirmedPassword: 'pzrj',
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp); 
+    //   }
+    // });
   }
 }
 </script>
