bf42adf61493b0a4810494b759b706f7b7e86741
Rita Zhang <ruijia1217@gmail.com>
2022-10-03 17:18:51 -0600

    CRUD Bot API for personal center page

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,13 +6,90 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex';
 
 export default {
     components: {
         ContentField
+    },
+
+    setup() {
+        const store = useStore();
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:8080/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "bot's title",
+        //         description: "bot's description",
+        //         content: "bot's content",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:8080/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 10,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:8080/user/bot/update/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 9,
+        //         title: "bot's title10",
+        //         description: "bot's description10",
+        //         content: "bot's content10",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+
+        $.ajax({
+            url: "http://127.0.0.1:8080/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
     }
 }
 </script>
 
 <style scoped> 
 </style>
+
