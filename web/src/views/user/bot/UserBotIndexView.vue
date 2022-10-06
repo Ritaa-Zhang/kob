2ff203ff338c752b222a7592a56e861b67e59c34
Ruijia Zhang <ruijia@RuijiadeMac-Studio.local>
2022-10-06 17:24:31 -0600

    user can create a bot

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -12,7 +12,52 @@
                 <div class="card" style="margin-top: 20px;">
                     <div class="card-header">
                         <span style="font-size: 130%;">My Bots</span>
-                        <button type="button" class="btn btn-primary float-end">Create</button>
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal"
+                            data-bs-target="#add-bot-btn">
+                            Create
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="add-bot-btn" tabindex="-1">
+                            <div class="modal-dialog modal-xl">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Create a bot</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <div class="mb-3">
+                                            <label for="add-bot-title" class="form-label">Title</label>
+                                            <input v-model="botadd.title" type="text" class="form-control"
+                                                id="add-bot-title" placeholder="Please enter bot's name.">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="add-bot-description" class="form-label">Description</label>
+                                            <textarea v-model="botadd.description" class="form-control"
+                                                id="add-bot-description" rows="3"
+                                                placeholder="Describe your bot."></textarea>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="add-bot-code" class="form-label">Code</label>
+                                            <textarea v-model="botadd.content" class="form-control" id="add-bot-code"
+                                                rows="7" placeholder="Please code for bot."></textarea>
+                                        </div>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <div class="error-message">{{ botadd.error_message}}</div>
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" @click="add_bot">Create</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div class="card-body">
                         <table class="table table-striped table-hover">
@@ -28,7 +73,8 @@
                                     <td>{{ bot.title }}</td>
                                     <td>{{ bot.createtime }}</td>
                                     <td>
-                                        <button type="button" class="btn btn-secondary" style="margin-right: 10px;">Modify</button>
+                                        <button type="button" class="btn btn-secondary"
+                                            style="margin-right: 10px;">Modify</button>
                                         <button type="button" class="btn btn-danger">Delete</button>
                                     </td>
                                 </tr>
@@ -42,14 +88,22 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import $ from 'jquery'
 import { useStore } from 'vuex';
+import { Modal } from 'bootstrap/dist/js/bootstrap';
 
 export default {
     setup() {
         const store = useStore();
         let bots = ref([]);
+
+        const botadd = reactive({
+            title: "",
+            description: "",
+            content: "",
+            error_message: "",
+        });
 
         const refresh_bots = () => {
             $.ajax({
@@ -66,13 +120,46 @@ export default {
 
         refresh_bots();
 
+        const add_bot = () => {
+            botadd.error_message = "";
+
+            $.ajax({
+                url: "http://127.0.0.1:8080/user/bot/add/",
+                type: "post",
+                data: {
+                    title: botadd.title,
+                    description: botadd.description,
+                    content: botadd.content,
+                },
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    if (resp.error_message === "success") {
+                        botadd.title = "";
+                        botadd.description = "";
+                        botadd.content = "";
+                        Modal.getInstance("#add-bot-btn").hide();
+                        refresh_bots();
+                    } else {
+                        botadd.error_message = resp.error_message;
+                    }
+                },
+            })
+        }
+
         return {
-            bots
+            bots,
+            botadd,
+            add_bot,
         }
     }
 }
 </script>
 
-<style scoped> 
+<style scoped>
+div.error-message {
+    color: red;
+}
 </style>
 
