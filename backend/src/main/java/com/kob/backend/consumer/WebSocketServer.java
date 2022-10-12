8db3ab2ebd2d3a9248b562b092fa5b6d9ab6b036
Ruijia Zhang <ruijia1217@gmail.com>
2022-10-12 17:05:25 -0600

    Synchronize player position and game logic (multi-threaded), complete results panel and restart game

--- backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -20,13 +20,14 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint("/websocket/{token}")
 public class WebSocketServer {
 
-    final private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    final public static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
     final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
 
     private User user;
     private Session session = null;
 
     private static UserMapper userMapper;
+    private Game game = null;
 
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
@@ -67,28 +68,51 @@ public class WebSocketServer {
             matchpool.remove(a);
             matchpool.remove(b);
 
-            Game game = new Game(13,14, 20);
+            Game game = new Game(13,14, 20, a.getId(), b.getId());
             game.createMap();
+
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+
+            game.start();
+
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id", game.getPlayerA().getId());
+            respGame.put("a_sx", game.getPlayerA().getSx());
+            respGame.put("a_sy", game.getPlayerA().getSy());
+            respGame.put("b_id", game.getPlayerB().getId());
+            respGame.put("b_sx", game.getPlayerB().getSx());
+            respGame.put("b_sy", game.getPlayerB().getSy());
+            respGame.put("map", game.getG());
 
             JSONObject respA = new JSONObject();
             respA.put("event", "start-matching");
             respA.put("opponent_username", b.getUsername());
             respA.put("opponent_photo", b.getPhoto());
-            respA.put("gamemap", game.getG());
-            users.get(a.getId()).sendMessgae(respA.toJSONString());
+            respA.put("game", respGame);
+            users.get(a.getId()).sendMessage(respA.toJSONString());
 
             JSONObject respB = new JSONObject();
             respB.put("event", "start-matching");
             respB.put("opponent_username", a.getUsername());
             respB.put("opponent_photo", a.getPhoto());
-            respB.put("gamemap", game.getG());
-            users.get(b.getId()).sendMessgae(respB.toJSONString());
+            respB.put("game", respGame);
+            users.get(b.getId()).sendMessage(respB.toJSONString());
         }
     }
 
     private void stopMatching() {
         System.out.println("Stop matching");
         matchpool.remove(this.user);
+    }
+
+    private void move(int direction) {
+        System.out.println(direction);
+        if (game.getPlayerA().getId().equals(user.getId())) {
+            game.setNextStepA(direction);
+        } else if (game.getPlayerB().getId().equals(user.getId())) {
+            game.setNextStepB(direction);
+        }
     }
 
     @OnMessage
@@ -102,6 +126,8 @@ public class WebSocketServer {
             startMatching();
         } else if ("stop-matching".equals(event)) {
             stopMatching();
+        } else if ("move".equals(event)) {
+            move(data.getInteger("direction"));
         }
     }
 
@@ -110,7 +136,7 @@ public class WebSocketServer {
         error.printStackTrace();
     }
 
-    public void sendMessgae(String message) {
+    public void sendMessage(String message) {
         synchronized (this.session) {
             try {
                 this.session.getBasicRemote().sendText(message);
--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -60,17 +60,17 @@ export class Snake extends GameObject {
         this.eye_direction = d;
         this.direction = -1;
         this.status = "move";
-        this.step ++ ;
+        this.step++;
 
         const k = this.cells.length;
         // Each element of the array is moved back one position.The first element is unchanged.
-        for (let i = k; i > 0; i -- ) {
+        for (let i = k; i > 0; i--) {
             this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
         }
 
-        if (!this.gamemap.check_valid(this.next_cell)) {
-            this.status = "die";
-        }
+        // if (!this.gamemap.check_valid(this.next_cell)) {
+        //     this.status = "die";
+        // }
     }
 
     update_move() {
@@ -126,7 +126,7 @@ export class Snake extends GameObject {
             ctx.fill();
         }
 
-        for (let i = 1; i < this.cells.length; i ++ ) {
+        for (let i = 1; i < this.cells.length; i++) {
             const a = this.cells[i - 1], b = this.cells[i];
             if (Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps)
                 continue;
@@ -138,7 +138,7 @@ export class Snake extends GameObject {
         }
 
         ctx.fillStyle = "black";
-        for (let i = 0; i < 2; i ++ ) {
+        for (let i = 0; i < 2; i++) {
             const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L;
             const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.15) * L;
             ctx.beginPath();
